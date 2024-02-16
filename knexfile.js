9c6ebee96f14a304bec5569a2d2b3c3971349c8b
Katie <kaw113@ucsd.edu>
2024-02-16 01:57:30 -0800

    change production setting

--- knexfile.js
+++ knexfile.js
@@ -23,8 +23,11 @@ module.exports = {
   },
 
   production: {
-    client: "postgresql",
-    connection: DATABASE_URL,
+    client: "pg",
+    connection: {
+      connectionString: DATABASE_URL,
+      ssl: { rejectUnauthorized: false }
+    },
     pool: { min: 0, max: 5 },
     migrations: {
       directory: path.join(__dirname, "src", "db", "migrations"),
